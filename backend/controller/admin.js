716ed3e4b4aaec9c27116a974d8f5df7de539ac0
faris <mrfaristp@gmail.com>
2023-08-23 11:21:42 +0530

    login auth created

--- backend/controller/admin.js
+++ backend/controller/admin.js
@@ -3,11 +3,28 @@ import adminModel from "../model/adminModel.js";
 import userModel from "../model/userModel.js";
 import jwt from "jsonwebtoken";
 
-var salt = bcrypt.genSaltSync(10);
-
 export async function adminLogin(req, res) {
   try {
     const { email, password } = req.body;
+    const user = await adminModel.find({ email });
+    if (user) {
+      const valid = bcrypt.compareSync(password, user[0].password);
+      if (valid) {
+        const key = process.env.SECRECT_KEY;
+        const token = jwt.sign({ userId: user[0]._id }, key, {
+          expiresIn: "1h",
+        });
+        res.cookie("token", token, {
+          httpOnly: true,
+          maxAge: 3600000,
+        });
+        res.status(200).json({ message: "success" ,user ,token});
+      } else {
+        res.json({ message: "password incorrect" });
+      }
+    } else {
+      res.json({ message: "admin not found" });
+    }
   } catch (error) {
     console.log(error);
   }
@@ -15,20 +32,20 @@ export async function adminLogin(req, res) {
 
 export async function getUser(req, res) {
   try {
-    const user = await userModel.find().sort({ email: -1 });
+    const user = await userModel.find().sort({ createdAt: -1 });
     res.send(user);
   } catch (error) {
     console.log(error);
   }
 }
 
-export async function deleteUser(req,res){
-    try {
-        const user = await userModel.deleteOne({_id :req.params.id})
-        if (user) {
-            res.send(true)
-        }
-    } catch (error) {
-        console.log(error);
+export async function deleteUser(req, res) {
+  try {
+    const user = await userModel.deleteOne({ _id: req.params.id });
+    if (user) {
+      res.send(true);
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
--- frontend/src/components/adminLogin.js
+++ frontend/src/components/adminLogin.js
@@ -1,33 +1,37 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const AdminLogin = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errMsg , setErrMsg] = useState('');
+
+  const navigate = useNavigate()
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     try {
       const response = await axios.post('/admin-login', { email, password }); 
+      setErrMsg(response.data.message)
       if (response.status === 200) {
-        // Admin logged in successfully
-        // You can store the token in local storage or a cookie here
+        navigate('/dashboard')
       }
     } catch (error) {
-      console.log(error);
+      console.log(error ,'axios err');
     }
   };
 
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="bg-white p-8 rounded shadow-md w-full sm:w-96">
+      <div className="bg-gray-100 p-8 border-blue-100 border-2 rounded shadow-xl w-full sm:w-96">
         <h2 className="text-2xl font-semibold mb-4">Admin Login</h2>
         <form onSubmit={handleLogin}>
           <div className="mb-4">
             <label className="block font-medium mb-1">Email:</label>
             <input
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-2 border-2 rounded focus:outline-none focus:border-blue-500"
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -37,7 +41,7 @@ const AdminLogin = () => {
           <div className="mb-4">
             <label className="block font-medium mb-1">Password:</label>
             <input
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
+              className="w-full px-3 py-2 border-2 rounded focus:outline-none focus:border-blue-500"
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
@@ -50,6 +54,7 @@ const AdminLogin = () => {
           >
             Login
           </button>
+          {errMsg && <p className='text-red-600 text-center mt-2'>{errMsg} &nbsp;</p>}
         </form>
       </div>
     </div>
